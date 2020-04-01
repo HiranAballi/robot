4220e3a6521dfc1d53914d6ecee717aeccd4b1e6
HiranAballi <hiranaballi3@gmail.com>
2020-04-01 20:00:06 +0100

    Updates

---
+++ precache-manifest.8ad623c74c62f1edefca5539c4992905.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e0aad2bbda3e9e0f230cf19e29714bd",
+    "url": "/robot/index.html"
+  },
+  {
+    "revision": "0217e0fbb92d7b0f747b",
+    "url": "/robot/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "7318fe53106d9de3ce42",
+    "url": "/robot/static/css/main.46e6b58d.chunk.css"
+  },
+  {
+    "revision": "0217e0fbb92d7b0f747b",
+    "url": "/robot/static/js/2.f68723af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robot/static/js/2.f68723af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7318fe53106d9de3ce42",
+    "url": "/robot/static/js/main.1258320b.chunk.js"
+  },
+  {
+    "revision": "005f8ed061fe4ced6ece",
+    "url": "/robot/static/js/runtime-main.9fa087ce.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot/static/media/SEGA.12a3db8b.woff"
+  }
+]);
